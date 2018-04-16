81bea5ad40a094f9d62d8cdf1c36828a9495d152
Kyle Smith <kyle.smith@Pampaneria.local>
2018-04-16 12:25:20 -0400

    still working on front end implmentation.

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 
+process.env.PWD = process.cwd()
+
 var app = require('express')(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     ent = require('ent'), // Blocks HTML characters (security equivalent to htmlentities in PHP)
     fs = require('fs');
+var express = require("express");
 
 // Loading the page index.html
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+//var path = require('path');
+//var app1 = app();
+
+
+app.use(express.static(process.env.PWD + '/public'));
 
 io.sockets.on('connection', function (socket, username) {
     // When the username is received it’s stored as a session variable and informs the other people
